4fdb476cd289a41f3d271c5535bfc928ab36454b
JulianRo <julian-roling@versanet.de>
2021-05-31 21:01:46 +0200

    Änderungen 28./29.05

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_21_143219) do
+ActiveRecord::Schema.define(version: 2021_05_22_102542) do
 
   create_table "armies", force: :cascade do |t|
     t.bigint "user_id"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2021_05_21_143219) do
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "short_title"
   end
 
   create_table "games_users", force: :cascade do |t|
