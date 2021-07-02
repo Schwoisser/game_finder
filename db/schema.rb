db3b0220786f93c66a4d9326a04bfff08f80cd80
JulianRo <julian-roling@versanet.de>
2021-07-02 11:13:58 +0200

    schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -109,7 +109,7 @@ ActiveRecord::Schema.define(version: 2021_06_18_133058) do
   end
 
   create_table "player_groups", force: :cascade do |t|
-    t.bigint "organizer_id"
+    t.bigint "creator_id"
     t.text "title"
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
