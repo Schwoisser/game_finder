f60f12869b9ab76c84cfb4f46ba7dbf67b45577c
Milan Pogadl <milan298@gmx.de>
2021-10-01 17:22:11 +0200

    play screen

--- app/controllers/player_finder_controller.rb
+++ app/controllers/player_finder_controller.rb
@@ -1,6 +1,23 @@
 class PlayerFinderController < ApplicationController
   def index
-    @users = User.by_distance(:origin => current_user)[1..13]
-    @matches = Match.by_distance(:origin => current_user)[1..13]
+
+    # TODO get game system oarams if there otherise current_user. games.first
+    if params[:id]
+      Rails.logger.info(params[:id])
+      selected_game = Game.find(params[:id].to_i)
+      @test ="with params id"
+    else
+      selected_game = current_user.games.first
+    end
+    Rails.logger.info(selected_game)
+    @users = User.joins(:games).where(games: [selected_game]).by_distance(:origin => current_user)[1..13] || []
+    @matches = Match.where(game: selected_game).by_distance(:origin => current_user)[1..13] || []
+    Rails.logger.info(@users)
+    Rails.logger.info(@matches)
+    # # Turbo stream response or html
+    respond_to do |format|
+      format.turbo_stream
+      format.html
+    end
   end
 end
---
+++ app/javascript/controllers/toggle_controller.js
@@ -0,0 +1,20 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+    static targets = ["toggled"];
+
+    connect() {
+      // for (let index = 0; index < this.toggledTargets.length; index++) {
+      //   const element = this.toggledTargets[index]; 
+      //   console.log(element)
+      // }
+    }
+
+    toggle() {
+      for (let index = 0; index < this.toggledTargets.length; index++) {
+        const element = this.toggledTargets[index]; 
+        // console.log(element)
+        $(element).toggleClass("hidden");
+      }
+    }
+}
