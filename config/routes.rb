0f96d3e7924b6109d013183a1a027a156f81af18
Milan Pogadl <milan298@gmx.de>
2021-10-23 11:12:00 +0200

    fonts and design

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   root "main#index"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get "elements", to: "main#elements"
   
   get "/profile", to: "profile#show"
   # Reihenfolge beachten
