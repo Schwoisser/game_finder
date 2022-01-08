c84adaaa848843b92eac9b1c9e55575b772f4454
Milan Pogadl <milan298@gmx.de>
2022-01-08 16:17:28 +0100

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2022_01_08_141713) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
@@ -40,9 +39,6 @@ ActiveRecord::Schema.define(version: 2022_01_08_141713) do
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
-=======
-ActiveRecord::Schema.define(version: 2021_12_28_153204) do
->>>>>>> 5076095eab66d60b35ecb4e18b4cf9964a53fd93
 
   create_table "armies", force: :cascade do |t|
     t.integer "user_id"
@@ -117,6 +113,7 @@ ActiveRecord::Schema.define(version: 2021_12_28_153204) do
     t.integer "user_id"
     t.string "status", default: "open"
     t.integer "max_player_number", default: 2000
+    t.string "address", limit: 2000
     t.string "country", limit: 2000
     t.string "city", limit: 2000
     t.string "street", limit: 2000
@@ -132,11 +129,11 @@ ActiveRecord::Schema.define(version: 2021_12_28_153204) do
   end
 
   create_table "matches_users", force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "match_id"
+    t.integer "user_id"
+    t.integer "match_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "match_score"
+    t.integer "match_score"
     t.boolean "accepted", default: false
     t.boolean "rated", default: false
   end
